12731e2bfc41112c1073735804067b273148749c
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-16 17:54:25 +0545

    Importing express and used in typescript

--- App.ts
+++ App.ts
@@ -1,13 +1,8 @@
 import { readFile } from 'fs';
 import { createServer } from 'http';
 
-var server = createServer((req, resp) => {
-    readFile('index.html', (err, data) => {
-        resp.write(data.toString());
-        resp.end();
-    });
-});
-
-server.listen(9000, () => {
-    console.log("server running");
-});
+import express = require('express');
+var app = express();
+app.get('/', function (req, resp) {
+    resp.send('aaa');
+})
